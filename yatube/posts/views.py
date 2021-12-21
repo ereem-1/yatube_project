0100eaa5131eb3340172072ae3e78825eb7e1ce3
Данила Еремин <d.ereomin@yandex.ru>
2021-12-21 23:43:48 +0300

    добавлен словарь context

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница')
-
-
-"""def group_posts(request):
-    return HttpResponse('Список мороженого')"""
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Главная страница',
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts(request, slug):
-    return HttpResponse(f'Пост: {slug}')
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Главная страница',
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
